558cd2f14ea8230b899dcb7778ae7ef07b7d8ef2
lauanyrb <lauannyrodrigues05@gmail.com>
2023-10-22 19:44:42 -0300

    feat: alterando o input

--- login/index-login.php
+++ login/index-login.php
@@ -15,19 +15,19 @@
       <div> 
         <img src="./img/logo-sem-fundo.png">
       </div>
-      <form method="post">
-        <span>Nome de usuário</span>
-        <input type="text" name="nome" placeholder="Digite o seu nome de usuário" autofocus>
-        <span>Senha</span>
-        <input type="password" senha="senha" placeholder="Digite sua senha...">
+      <form action="" method="post">
+        <label>E-mail</label>
+        <input type="email" name="email" placeholder="Digite o seu e-mail" autofocus>
+        <label>Senha</label>
+        <input type="password" senha="senha" placeholder="Digite a sua senha">
         <input type="submit" value="entrar" onclick="redirecionar(event)">
       </form>
       <p>Ainda não tem um conta?<a href="./cadastro/index-cadastro.html">Criar conta</a></p> 
     </div>
     <h1>
       <?php 
-        echo "OIIIIIIIIIII";
-      ?>
+        echo "";
+      ?> 
     </h1>
   </section>
 
