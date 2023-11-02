983d28ed2397467b111547dc61cf6fb805ee0d46
CaycCustodio <cayccustodio2001@gmail.com>
2023-11-02 15:14:55 -0300

    Remover itens carrinho

--- TesteFinal/carrinho.php
+++ TesteFinal/carrinho.php
@@ -1,14 +1,30 @@
 <?php
 session_start();
 
+if(isset($_POST['deletar'])){
+    //echo $_POST['indice']; // Exibe o índice do usuário que está sendo excluído
+    unset($_SESSION['carrinho'][$_POST['indice']]); // Remove o usuário da sessão com base no índice recebido via POST
+}
+
 if (isset($_SESSION['carrinho']) && !empty($_SESSION['carrinho'])) {
     echo "<h1>Seu Carrinho de Compras</h1>";
 
-    foreach ($_SESSION['carrinho'] as $produto) {
+    foreach ($_SESSION['carrinho'] as $key => $produto) {
+       
+        echo "<form action='' method='post'>"; 
+
         echo "<div class='item-carrinho'>";
+        echo "<img src='" . $produto['imagem'] . "' alt='Imagem do Produto'>";
         echo "<h2>" . $produto['nome'] . "</h2>";
         echo "<p>Preço: R$ " . $produto['preco'] . "</p>";
+        echo "<td><input type='submit' name ='deletar' value='D'/></td>";// Botão para excluir o produto
+        
+        echo "<input type='hidden' name='indice' value='$key'/>"; // Campo oculto com o índice do usuário
+
         echo "</div>";
+
+        echo "</form>";
+
     }
 
     // Calcula o total dos itens no carrinho
