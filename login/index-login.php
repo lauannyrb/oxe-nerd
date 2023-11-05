84e4b3d65a730bf4264361ea5de5a838e0fd7d35
lauanyrb <lauannyrodrigues05@gmail.com>
2023-11-05 16:26:18 -0300

    feat: login

--- login/index-login.php
+++ login/index-login.php
@@ -7,7 +7,7 @@
   <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
   <link rel="stylesheet" href="style-login.css">
   <link rel="icon" href="../images/Logo.svg" type="svg">
-  <title>login</title>
+  <title>Login</title>
 </head>
 <body>
   <section class="area-login">
@@ -15,22 +15,27 @@
       <div> 
         <img src="./img/logo-sem-fundo.png">
       </div>
-      <form method="post">
-        <span>Nome de usuário</span>
-        <input type="text" name="nome" placeholder="Digite o seu nome de usuário" autofocus>
-        <span>Senha</span>
-        <input type="password" senha="senha" placeholder="Digite sua senha...">
-        <input type="submit" value="entrar" onclick="redirecionar(event)">
+      
+      <form method="post" action="processa_login.php">
+        <?php
+        session_start();
+        if (isset($_SESSION['login_erro'])) {
+            echo '<div class="mensagem-erro">' . $_SESSION['login_erro'] . '</div>';
+            unset($_SESSION['login_erro']); // Limpar a mensagem de erro
+        }
+        ?>
+
+        <label>E-mail</label>
+        <input type="email" name="email" placeholder="Digite o seu e-mail" autofocus>
+        <label>Senha</label>
+        <input type="password" name="senha" placeholder="Digite a sua senha">
+        <input type="submit" value="Entrar">
       </form>
       <p>Ainda não tem um conta?<a href="./cadastro/index-cadastro.php">Criar conta</a></p> 
     </div>
+    <h1>
+    </h1>
   </section>
 
-  <script>
-    function redirecionar(event) {
-      event.preventDefault(); // Impede o envio do formulário
-      window.location.href = "../index.php";
-    }
-  </script>
 </body>
 </html>
