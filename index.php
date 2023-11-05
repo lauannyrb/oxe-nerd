b981fd566ad1b87f96e6536b086a2e2ba7d16090
lauanyrb <lauannyrodrigues05@gmail.com>
2023-11-05 16:25:01 -0300

    feat: ajuste nos links para as imagens no caarinho

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <section id="promocoes" class="carrossel">
         <section class="container">
-            <img class="venda" src="images/tecladin_removebg_preview_8.png" alt="Imagem de venda">
+            <img class="venda" src="./images/tecladin_removebg_preview_8.png" alt="Imagem de venda">
             <h2>Conjunto camisa namorados GAMER</h2>
             <p><s>R$ 65,50</s></p>
             <p class="preco"> <strong>R$ 45,50</strong></p>
@@ -92,7 +92,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 <form method="post">
                     <input type="hidden" name="nome" value="Conjunto camisa namorados GAMER">
                     <input type="hidden" name="preco" value="45.50"> <!-- Substitua pelo preço do produto -->
-                    <input type="hidden" name="imagem" value="images/tecladin_removebg_preview_8.png"> <!-- Substitua pelo caminho da imagem do produto -->
+                    <input type="hidden" name="imagem" value="../images/tecladin_removebg_preview_8.png"> <!-- Substitua pelo caminho da imagem do produto -->
                     <button class="btn" type="submit" name="comprar">COMPRAR </button>
                 </form>
             </div>
@@ -108,7 +108,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <form method="post">
                         <input type="hidden" name="nome" value="Star Guardian Orianna Cosplay Traje League of Legends">
                         <input type="hidden" name="preco" value="45.50"> <!-- Substitua pelo preço do produto -->
-                        <input type="hidden" name="imagem" value="./images/vestido.png"> <!-- Substitua pelo caminho da imagem do produto -->
+                        <input type="hidden" name="imagem" value="../images/vestido.png"> <!-- Substitua pelo caminho da imagem do produto -->
                         <button class="btn" type="submit" name="comprar">COMPRAR </button>
                     </form>
                 </div>
@@ -124,7 +124,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <form method="post">
                         <input type="hidden" name="nome" value="Placa de Vídeo PNY GeForce RTX 4090 XLR8">
                         <input type="hidden" name="preco" value="6999.99"> <!-- Substitua pelo preço do produto -->
-                        <input type="hidden" name="imagem" value="images/rtx.png"> <!-- Substitua pelo caminho da imagem do produto -->
+                        <input type="hidden" name="imagem" value="../images/rtx.png"> <!-- Substitua pelo caminho da imagem do produto -->
                         <button class="btn" type="submit" name="comprar">COMPRAR </button>
                     </form>
                 </div>            
@@ -140,7 +140,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <form method="post">
                         <input type="hidden" name="nome" value="Caneca GAMER personalizada">
                         <input type="hidden" name="preco" value="15.99"> <!-- Substitua pelo preço do produto -->
-                        <input type="hidden" name="imagem" value="images/Caneca.png"> <!-- Substitua pelo caminho da imagem do produto -->
+                        <input type="hidden" name="imagem" value="../images/Caneca.png"> <!-- Substitua pelo caminho da imagem do produto -->
                         <button class="btn" type="submit" name="comprar">COMPRAR </button>
                     </form>
                 </div>            
