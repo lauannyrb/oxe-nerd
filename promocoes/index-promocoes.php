08d326a5b64d395ab7f6229df466c0626a7c226d
Jonas Oliveira Dos Santos <jonasoliveiradossantos@outlook.com.br>
2023-11-05 23:24:11 -0300

    nunca falem cmg
    odeio

--- promocoes/index-promocoes.php
+++ promocoes/index-promocoes.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <a href="../index.php"><img class="logo-oxe-nerd" src="../images/oxe-nerd-logo.png" title="Logo da Oxe Nerd"></a>        
         <nav>
             <a class="" href="../produtos/cadastro_produtos.html"> Novos produtos </a>
-            <a class="" href="#"> Promoções </a>
+            <a class="" href="../promocoes/index-promocoes.php"> Promoções </a>
             <a class="" href="../eletronicos/index-eletronicos.php"> Eletrônicos </a>
             <a class="" href="../personalizados/index-personalizados.php"> Personalizados </a>
             <a class="" href="../login/index-login.php"> Login </a>
