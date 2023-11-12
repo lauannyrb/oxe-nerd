2e99bbb6f9f0aeee254039b5761c29911715db5a
lauanyrb <lauannyrodrigues05@gmail.com>
2023-11-12 17:50:54 -0300

    feat: ajuste em redirecionamento para a tela de estilização

--- perfil.php
+++ perfil.php
@@ -23,7 +23,7 @@ $email_usuario = $_SESSION['usuario_logado']['email'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OXE NERD</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./perfil.css">
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="icon" href="images/Logo.svg" type="svg">
 </head>
