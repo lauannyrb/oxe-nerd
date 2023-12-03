e47147b3ab8a01130e38a25f8602dc13273f2f87
lauanyrb <lauannyrodrigues05@gmail.com>
2023-12-03 19:26:29 -0300

    conexao com banco

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = ""; 
+$dbname = "oxe-nerd";
+
+// Criar conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
+}
+?>
