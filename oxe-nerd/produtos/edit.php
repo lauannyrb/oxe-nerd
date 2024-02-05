5c65f3a5f1042553c74b560bde35ddd7ade5708b
Your Name <you@example.com>
2024-02-05 09:21:30 -0300

    foi

--- oxe-nerd/produtos/edit.php
+++ oxe-nerd/produtos/edit.php
@@ -3,14 +3,15 @@ include '../conexao.php';
 sessao();
 logout();
 verificarAdm();
+
 if(isset($_POST['deletar'])){
-    //echo $_POST['indice']; // Exibe o índice do usuário que está sendo excluído
-    unset($_SESSION['produtos'][$_POST['indice']]); // Remove o usuário da sessão com base no índice recebido via POST
+    // Remove o produto da sessão com base no índice recebido via POST
+    unset($_SESSION['produtos'][$_POST['indice']]);
 }
 
 if(isset($_POST['editar'])){
-    echo $_POST['indice']; // Exibe o índice do usuário que está sendo editado
-    header('Location: editprodt.php?id='.$_POST['indice']); // Redireciona para a página "editarUsuario.php" com o ID do usuário que será editado na URL
+    // Redireciona para a página de edição com o ID do produto que será editado na URL
+    header('Location: editprodt.php?id='.$_POST['indice']);
 }
 
 // Verificar se o usuário está logado
@@ -19,7 +20,6 @@ $nome_usuario = "Faça login";
 if (isset($_SESSION['usuario_logado']) && is_array($_SESSION['usuario_logado'])) {
     $nome_usuario = $_SESSION['usuario_logado']['nome'];
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -47,14 +47,14 @@ if (isset($_SESSION['usuario_logado']) && is_array($_SESSION['usuario_logado']))
 </header>
     <!-- Fim  -->
 
-<section> <h1> Lista de produtos </h1>  <section>
+<section>
+    <h1> Lista de produtos </h1>
+</section>
     
 <main class="description">
     <div class="grid">
 
     <?php
-    // Configurações do banco de dados
-
     // Verifica se a conexão foi bem sucedida
     if ($conn->connect_error) {
         die("Erro na conexão: " . $conn->connect_error);
@@ -67,44 +67,47 @@ if (isset($_SESSION['usuario_logado']) && is_array($_SESSION['usuario_logado']))
     if ($result->num_rows > 0) {
         // Exibindo formulário para editar cada produto
         while($row = $result->fetch_assoc()) {
-            ?>
+    ?>
+        <div class="centralizar">
             <form action="editar_produto.php" method="post" enctype="multipart/form-data">
-                <div class="centralizar">
-                    <div class="titulos">
+                <div class="titulos">
                     <h3><?php echo $row['name']; ?></h3>
-                    </div>
-                    <div class="picture">
-                        <label for="image_path" class="text">Imagem Atual:</label> 
-                        <img class="img" src="<?php echo $row['image_path']; ?>" alt="Imagem atual"><!-- Exibir imagem atual -->
-                        <label for="new_image" class="text">Nova Imagem:</label> 
-                        <div class="new-selector">
-                            <input type="file" class="newimg" name="new_image"><!-- Campo para carregar nova imagem -->
-                        </div>
-                    </div>
-                    <div class="lista">
-                        <input type="hidden" name="id" class="result" value="<?php echo $row['id']; ?>"> 
-                        <label for="name" class="text">Nome:</label> 
-                        <input type="text" class="result" name="name" class="result" value="<?php echo $row['name']; ?>">
-                        <label for="price" class="text">Preço:</label> 
-                        <input type="text" name="price" class="result" value="<?php echo $row['price']; ?>">
-                        <label for="old_price" class="text">Preço Antigo:</label> 
-                        <input type="text" name="old_price" class="result" value="<?php echo $row['old_price']; ?>">
-                        <label for="category" class="text">Categoria:</label> 
-                        <input type="text" name="category" class="result" value="<?php echo $row['category']; ?>">
-                        <label for="quantidade" class="text">Quantidade:</label> 
-                        <input type="text" name="quantidade" class="result" value="<?php echo $row['quantidade']; ?>">
-                    </div>
-                    <div class="botoes">
-                    <input type="submit" value="Salvar" class="btn">
-                    <form action="excluir_produto.php" method="post">
-                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
-                        <button type="submit" class="btn2" name="apagar">Apagar</button><!-- Botão para apagar -->
-                    </form>
-                    </div>
-
                 </div>
+                <div class="picture">
+                    <label for="image_path" class="text">Imagem Atual:</label> 
+                    <img class="img" src="<?php echo $row['image_path']; ?>" alt="Imagem atual"><!-- Exibir imagem atual -->
+                    <label for="new_image" class="text">Nova Imagem:</label> 
+                    <div class="new-selector">
+                        <input type="file" class="newimg" name="new_image"><!-- Campo para carregar nova imagem -->
+                    </div>
+                </div>
+                <div class="lista">
+                    <input type="hidden" name="id" class="result" value="<?php echo $row['id']; ?>"> 
+                    <label for="name" class="text">Nome:</label> 
+                    <input type="text" class="result" name="name" class="result" value="<?php echo $row['name']; ?>">
+                    <label for="price" class="text">Preço:</label> 
+                    <input type="text" name="price" class="result" value="<?php echo $row['price']; ?>">
+                    <label for="old_price" class="text">Preço Antigo:</label> 
+                    <input type="text" name="old_price" class="result" value="<?php echo $row['old_price']; ?>">
+                    <label for="category" class="text">Categoria:</label> 
+                    <input type="text" name="category" class="result" value="<?php echo $row['category']; ?>">
+                    <label for="quantidade" class="text">Quantidade:</label> 
+                    <input type="text" name="quantidade" class="result" value="<?php echo $row['quantidade']; ?>">
+                </div>
+                <div class="botoes">
+                    <input type="submit" value="Salvar" class="btn">
+                    </form>
+                    <form action="excluir_produto.php" method="post">
+                
+                    <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                    <button type="submit" class="btn2" name="apagar">Apagar</button><!-- Botão para apagar -->
+                
             </form>
-            <?php
+                </div>
+            
+            
+        </div>
+    <?php
         }
     } else {
         echo "0 resultados";
@@ -117,17 +120,15 @@ if (isset($_SESSION['usuario_logado']) && is_array($_SESSION['usuario_logado']))
     </div>
 </main>
 
- <!---------------- Fale Conosco incio ---------------->
- <footer>
-        <h2>Fale Conosco</h2>
-        <div>
-            <img src="../images/Whatsapp.png" alt="Whatsapp"><p>82 99714-3090</p>
-            <img src="../images/Instagram.png" alt="Instagram"><p>@oxe_nerd</p>
-            <img src="../images/Mail.png" alt="E-Mail"><p>oxenerdbr@outlook.com</p>
-        </div>
-        <p><strong>OXE NERD<BR>Todos os direitos reservados</strong></p> 
-    </footer>
-    <!---------------- Fale Conosco fim ---------------->
+<footer>
+    <h2>Fale Conosco</h2>
+    <div>
+        <img src="../images/Whatsapp.png" alt="Whatsapp"><p>82 99714-3090</p>
+        <img src="../images/Instagram.png" alt="Instagram"><p>@oxe_nerd</p>
+        <img src="../images/Mail.png" alt="E-Mail"><p>oxenerdbr@outlook.com</p>
+    </div>
+    <p><strong>OXE NERD<BR>Todos os direitos reservados</strong></p> 
+</footer>
 
 </body>
 </html>
--- oxe-nerd/produtos/editar_produto.php
+++ oxe-nerd/produtos/editar_produto.php
@@ -16,19 +16,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $old_price = $_POST['old_price'];
     $category = $_POST['category'];
     $quantidade = $_POST['quantidade'];
+    $image_path = ''; // Definindo $image_path inicialmente como uma string vazia
 
     // Verifica se uma nova imagem foi enviada
-    if ($_FILES['new_image']['size'] > 0) {
+    if (isset($_FILES['new_image']) && $_FILES['new_image']['size'] > 0) {
         // Configurações da imagem
         $target_dir = "../produtos/uploads/"; // Diretório onde a imagem será armazenada
         $target_file = $target_dir . basename($_FILES["new_image"]["name"]); // Caminho completo do arquivo de destino
         $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
         // Verifica se o arquivo é uma imagem real ou um arquivo fake
-        if(isset($_POST["submit"])) {
+        if (isset($_POST["submit"])) {
             $check = getimagesize($_FILES["new_image"]["tmp_name"]);
-            if($check !== false) {
+            if ($check !== false) {
                 echo "Arquivo é uma imagem - " . $check["mime"] . ".";
                 $uploadOk = 1;
             } else {
@@ -37,39 +38,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
 
-        // // Verifica se o arquivo já existe
-        // if (file_exists($target_file)) {
-        //     echo "Desculpe, a imagem já existe.";
-        //     $uploadOk = 0;
-        // }
-
-        // // Verifica o tamanho do arquivo
-        // if ($_FILES["new_image"]["size"] > 500000) {
-        //     echo "Desculpe, o arquivo é muito grande.";
-        //     $uploadOk = 0;
-        // }
-
-        // // Permitir apenas alguns formatos de arquivo
-        // if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        // && $imageFileType != "gif" ) {
-        //     echo "Desculpe, apenas arquivos JPG, JPEG, PNG & GIF são permitidos.";
-        //     $uploadOk = 0;
-        // }
-
         // Verifica se $uploadOk está definido como 0 por algum erro
         if ($uploadOk == 0) {
             echo "Desculpe, sua imagem não foi enviada.";
         // Se tudo estiver correto, tenta fazer o upload do arquivo
         } else {
             if (move_uploaded_file($_FILES["new_image"]["tmp_name"], $target_file)) {
-                
                 // Atualiza o caminho da imagem no banco de dados
                 $image_path = $target_file;
-                header("Location: ../produtos/editar_produto.php"); // Redirecionar para a página inicial após o logout
-
             } else {
                 echo "Desculpe, ocorreu um erro ao enviar sua imagem.";
             }
+        }
+    } else {
+        // Se não houver envio de nova imagem, mantém o caminho da imagem existente
+        $sql_select = "SELECT image_path FROM products WHERE id = $id";
+        $result = $conn->query($sql_select);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $image_path = $row["image_path"];
+            }
+        } else {
+            echo "Produto não encontrado.";
+            exit; // Termina o script se o produto não for encontrado
         }
     }
 
@@ -84,7 +75,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             WHERE id = $id";
 
     if ($conn->query($sql) === TRUE) {
-        echo "Produto atualizado com sucesso";
+        header("Location: ../produtos/edit.php");
+        exit; // Termina o script após o redirecionamento
     } else {
         echo "Erro ao atualizar produto: " . $conn->error;
     }
@@ -93,6 +85,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $conn->close();
 } else {
     // Se os dados não foram enviados via método POST, redireciona para a página anterior ou exibe uma mensagem de erro
-    echo "Erro: Os dados do formulário não foram recebidos corretamente.";
+    header("Location: ../produtos/editar_produto.php"); 
+    exit; // Termina o script após o redirecionamento
 }
 ?>
